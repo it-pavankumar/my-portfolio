0679ab368dd84305471b2464b67fc8cf0bdfabaa
PAVAN6969-IT <inagapavankumar@gmail.com>
2025-01-20 22:18:48 -0800

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,21 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ExperienceComponent } from "./experience/experience.component";
 import { ContactComponent } from "./contact/contact.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [HomeComponent, AboutComponent, ExperienceComponent, ContactComponent]
+  imports: [HomeComponent, AboutComponent, ExperienceComponent, ContactComponent, CommonModule]
 })
 
 
 export class AppComponent {
   title = 'my-portfolio';
   isScrolled:boolean = false;
+  isNavbarCollapsed : boolean = false;
 
   @HostListener('window:scroll', [])
     onWindowScroll(): void {
@@ -26,5 +28,11 @@ export class AppComponent {
   scrollTo(sectionId: string): void {
     const ele = document.getElementById(sectionId);
     ele && ele.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+    console.log(this.isNavbarCollapsed);
+    
   }
 }
