eb48a845cfa8239d0249d95aa565be9a50796d82
PAVAN6969-IT <inagapavankumar@gmail.com>
2025-02-03 10:48:12 -0800

    updated FE

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,8 +11,8 @@ import { CommonServService } from '../common-serv.service';
 })
 export class AboutComponent {
 
-  cvPath: string = 'assets/CV_Naga Pavan Kumar_FS_Dev.pdf';
-  clPath: string = 'assets/CL_Naga Pavan Kumar_FS_Dev.pdf';
+  cvPath: string = 'assets/CV_NagaPKumar_FE_DEV.pdf';
+  clPath: string = 'assets/CL_NagaPKumar_FE_DEV.pdf';
 
   constructor(private _commonServ: CommonServService) {}
   
